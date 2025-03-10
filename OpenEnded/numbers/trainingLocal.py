b223a2768b54990f5e61d476444d7a9abc6a7eba
tmotluke <timothyng28@gmail.com>
2025-03-10 10:28:36 +0800

    create initial yolo training model for question number detection

---
+++ OpenEnded/numbers/trainingLocal.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+import torch
+device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+print(f"Using device: {device}")
+model = YOLO('yolo11x.pt')
+results = model.train(data="/Users/timothy/projects/Teebloc/OpenEnded/numbers/config-local.yaml", epochs=400, verbose = True, device = device, patience = 100)
